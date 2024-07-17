d391731b27c36b1c5c6b6f17254e3dc0f1e43a58
osparking <jbpark03@gmail.com>
2024-07-17 18:09:00 +0900

    김송아의 jpa udemy 강좌 구현 시작

---
+++ src/main/java/space/bum/songa/JpaSongaApplication.java
@@ -0,0 +1,13 @@
+package space.bum.songa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSongaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaSongaApplication.class, args);
+	}
+
+}

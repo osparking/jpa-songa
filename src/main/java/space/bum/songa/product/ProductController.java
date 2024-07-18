8f6aa670b88817cbae425b04da0727774b2f3bee
osparking <jbpark03@gmail.com>
2024-07-19 08:37:55 +0900

    만들어 놓은 데이터소스 참조 가져오기 - 제어기

--- src/main/java/space/bum/songa/product/ProductController.java
+++ src/main/java/space/bum/songa/product/ProductController.java
@@ -15,10 +15,11 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
-//    @GetMapping("/connectdb")
-//    public void makeConnection() {
-//        productService.makeConnection();
-//    }
+    @GetMapping("/connectdb")
+    public String makeConnection() {
+        productService.makeConnection();
+        return "connected";
+    }
 
     // 전체 상품 조회
     @GetMapping("/products")
